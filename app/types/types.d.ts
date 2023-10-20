d830e64ee54d1c3fd054f37545d752d3ccc1d607
Brayan Monge Méndez <bmongemendez@gmail.com>
2023-10-20 00:12:00 -0600

    Feat: Add types definitions and fix context types

---
+++ app/types/types.d.ts
@@ -0,0 +1,3 @@
+export interface Context {
+	isNavOpen: boolean;
+}
--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { Context } from "@/app/types/types";
 import { createContext, useMemo, useState } from "react";
 
 type Props = {
 	children: React.ReactNode;
 };
 
-export const appContext = createContext({});
+export const appContext = createContext<Context>({ isNavOpen: false });
 
 export default function AppContext({ children }: Props) {
 	const [isNavOpen, setIsNavOpen] = useState(false);
